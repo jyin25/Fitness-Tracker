8ca27444b1c79fdb5b7b7e4f430af500d59c6091
Jun <jyin25@gmail.com>
2019-11-14 16:24:13 -0800

    config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  API_ENDPOINT: 'https://nameless-spire-02434.herokuapp.com/api',
+  API_ENDPOINT: 'https://fit-database.herokuapp.com/api',
   TOKEN_KEY: 'auth-token',
-  DEMO_TOKEN_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjozLCJpYXQiOjE1NzM2Njk5ODMsInN1YiI6InRlc3QzIn0.ylnMKhl3ldN8eTrsRuO6Nv-w1Ar_n08khutdG7rLAXA'
+  DEMO_TOKEN_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxNiwiaWF0IjoxNTcyMjk4NjU5LCJzdWIiOiJqdW55aW4ifQ.w96EcXvY6jfPmf6Jc1dzj3bXfikOY6nwMWpqFjtA9qs'
 }
